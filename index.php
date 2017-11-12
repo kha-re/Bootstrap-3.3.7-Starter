93f648cbfe465eb47fd84c09a957039010a73fc5
Amit Kumar Khare <me.amitkhare@gmail.com>
2017-11-12 20:50:15 +0530

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<title>Website starter</title>
+    
+    <!-- bootstrap 3.3.7 css-->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" type="text/css" />
+
+    <!-- your own css-->
+    <link rel="stylesheet" href="./assets/css/styles.css" type="text/css" />
+</head>
+<body>
+    <h1>Try arain, try again, success!!</h1>
+    
+
+    <!-- jquery script -->
+    <script type="text/javascript" src="./assets/js/jquery.min.js"></script>
+    
+    <!-- Scripts for bootstrap 3.3.7 framework -->
+    <script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>
+    
+    <!-- your own javascript -->
+    <script type="text/javascript" src="./assets/js/scripts.js"></script>
+</body>
+</html>
